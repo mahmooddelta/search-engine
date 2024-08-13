6886dff58d332c7eece279e214b2019904fcdeb4
Mahmood <mahmooddelta01@gmail.com>
2024-08-13 22:51:25 +0330

    remove require files in index.php

--- index.php
+++ index.php
@@ -2,13 +2,6 @@
 require 'vendor/autoload.php';
 
 
-require './Src/Crawler.php';
-require './Src/SearchEngine.php';
-require './Src/SuggestionEngine.php';
-require './Src/WordProcessor.php';
-require './Src/DownloadFile.php';
-require './Src/UrlFetcher.php';
-require './Src/LinkExtractor.php';
 
 use App\Src\Crawler;
 use App\Src\LinkExtractor;
@@ -20,15 +13,14 @@ use App\Src\DownloadFile;
 use App\Interfaces\LinkExtractorInterface;
 use App\Interfaces\UrlFetcherInterface;
 
-// ساختن شیء از کلاس UrlFetcher
+
 $urlFetcher = new UrlFetcher();
-
-// ساختن شیء از کلاس LinkExtractor
 $linkExtractor = new LinkExtractor();
+$crawler = new Crawler("http://localhost/crawel/test", $urlFetcher, $linkExtractor);
+$pages = $crawler->crawl();
 
-// ساختن شیء از کلاس Crawler و پاس دادن ورودی‌ها
-$crawler = new Crawler("http://mortezaaminitabar.com", $urlFetcher, $linkExtractor);
-print_r($crawler->crawl());
+$wordProcessor = new WordProcessor($pages);
+print_r($wordProcessor->getUniqueWords());
 
 
 //$crawler = new Crawler("http://test.ir/");
