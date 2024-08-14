de3df7d4d43a3de03b6fbc9146ffd37f22ce9112
Mahmood <mahmooddelta01@gmail.com>
2024-08-14 18:03:35 +0330

    remove comments

--- Implementations/UrlFetcher.php
+++ Implementations/UrlFetcher.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Implementations;
 
 
@@ -8,11 +9,6 @@ class UrlFetcher implements UrlFetcherInterface
 {
     public function fetch(string $url): string
     {
-        $html = @file_get_contents($url);
-//        if ($html === false) {
-//            $error = error_get_last();
-//            throw new \Exception("Error fetching the URL: " . $error['message']);
-//        }
-        return $html;
+        return @file_get_contents($url);
     }
 }
